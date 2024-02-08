bfc8d6f183ff94fa52a0619c5fec1106fa1a2341
harshkumar3008 <158283384+harshkumar3008@users.noreply.github.com>
2024-02-08 10:19:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+# Python Day 1:-
+
+print("Hello World")
+print(5)
+print("5+3")    # Anything inside " " referred as String. so,it will exact printed.
+print(5+3)
+
+# Python Day 2:- ( project )
+
+  # Jarvis Virtual Assistant
+  # Love Calculator
+  # Face Recognition
+  # Flappy Bird Game
+  # Snake Game
+
+# Python Day 3:-
+
+# Python Day 4:-
+
+print("Hello World", 7)    # can take multiple value in single function( may be string or integer or float[2.0] )
+print(5)
+print("Bye")
+print(17*13)
+
+# Python Day 5:-           # is used to comment out. it is a part of code that doesn't execute.
+
+print("hey i am a good boy")
+print("hey i am a good boy"
+      "play")
+print("hey i am a good boy",         # , is used give space after boy in output.
+      "play")
+print("hey i am a good boy\nplay")   # \n (back slash n) is used to change the line of next code in output.
+                                     # \n is a escape sequence characters.
+print("hey i am a good boy"" play")  #
+                                     # space before play inside double quotes is used to give space in output after boy.
+                                     # ctrl + / (forward slash) is used to comment out multiple code at once after selecting this code.
+                                     # again pressing of ctrl + / is used to uncomment.
+''' hey my name is harsh kumar'''    # ''' is also used to comment out.
+"""hey my name is harsh kumar"""     # """ is also used to comment out.
+# print("hey iam a "good boy"\nand this viewer is also a good boy/girl")    #this code's output gives syntax error.
+                                     # here, python is confused about the string.
+print("hey iam a \"good boy\"\nand this viewer is also a good boy/girl")
+                                     # \"   \" is used to get part of string inside double quotes in output.
+print("hey iam a \'good boy\'\nand this viewer is also a good boy/girl")
+                                     # \'   \' is used to get part of string inside single quotes in output.
+
+print("hey", 6, 7, sep="~")          # sep=" " is a seperator, inside double quotes you can give any symbol.
+                                     # seperator is used to join all character inside the function.
+print("hey", 6, 7, sep="~", end=" 009")
+print("hey")                         # end=" " is used to next line of code in same line.
+print("hey", 6, 7, sep="~",end=" 009\n")           # end=" \n" is use to change the line.
+print("hey")
