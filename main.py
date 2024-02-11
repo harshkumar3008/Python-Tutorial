fd7db376439a49ec75da81022b821155cb0cdf0f
harshkumar3008 <158283384+harshkumar3008@users.noreply.github.com>
2024-02-11 10:30:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+a = "****** WELCOME TO CALCULATOR ******"
+print(a)
+
+x=input("Enter your first number:-")
+y=input("Enter your second number:-")
+
+print("The Sum of",x,"and",y,"is",int(x)+int(y))
+print("The Subtraction of",x,"and",y,"is",int(x)-int(y))
+print("The multiplication of",x,"and",y,"is",int(x)*int(y))
+print("The exponential of",x,"and",y,"is",int(x)**int(y))
+print("The division of",x,"and",y,"is",int(x)/int(y))
+print("The floor division of",x,"and",y,"is",int(x)//int(y))
+print("The modulus of",x,"and",y,"is",int(x)%int(y))
